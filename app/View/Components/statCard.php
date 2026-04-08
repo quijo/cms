44dac7ee01d51479fbd470814a22c2c087b560e0
John Quiachon <rjohnquia@gmail.com>
2026-04-09 07:35:49 +0800

    finalize stat card display givings

--- app/View/Components/statCard.php
+++ app/View/Components/statCard.php
@@ -2,23 +2,51 @@
 
 namespace App\View\Components;
 
-use Closure;
-use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class statCard extends Component
+class StatCard extends Component
 {
     public $title;
     public $value;
     public $icon;
     public $color;
+    public $model;
+    public $route;
+    public $typeFilter;
+    public $sumColumn;
 
-    public function __construct($title, $value, $icon = null, $color = 'blue')
-    {
+    public function __construct(
+        $title,
+        $model = null,
+        $icon = null,
+        $color = 'blue',
+        $route = null,
+        $typeFilter = null,
+        $sumColumn = null
+    ) {
         $this->title = $title;
-        $this->value = $value;
         $this->icon = $icon;
         $this->color = $color;
+        $this->route = $route;
+        $this->model = $model;
+        $this->typeFilter = $typeFilter;
+        $this->sumColumn = $sumColumn;
+
+        $this->value = 0;
+
+        if ($this->model && class_exists($this->model)) {
+            $query = $this->model::query();
+
+            if ($this->typeFilter) {
+                $query->where('type', $this->typeFilter);
+            }
+
+            if ($this->sumColumn) {
+                $this->value = $query->sum($this->sumColumn);
+            } else {
+                $this->value = $query->count();
+            }
+        }
     }
 
     public function render()
--- resources/views/components/stat-card.blade.php
+++ resources/views/components/stat-card.blade.php
@@ -1,22 +1,24 @@
-@props(['title', 'value', 'icon' => null, 'color' => 'blue'])
+{{-- @props(['title', 'value', 'icon', 'color', 'model', 'route']) --}}
 
-<div class="bg-white shadow-md rounded-2xl p-5 border-l-4 border-{{ $color }}-500">
-    
-    <div class="flex items-center justify-between">
-        
-        <div>
-            <h3 class="text-sm text-gray-500">{{ $title }}</h3>
-            <p class="text-2xl font-bold text-gray-800">{{ $value }}</p>
+@if($route)
+<a href="{{ route($route) }}" class="block">
+@endif
+
+<div class="flex flex-col items-center justify-center bg-white dark:bg-gray-800 shadow rounded-lg p-6 hover:shadow-lg transition-shadow cursor-pointer">
+    <!-- Icon -->
+    @if($icon)
+        <div class="text-{{ $color }}-500 mb-4">
+            {!! $icon !!}
         </div>
+    @endif
 
-        @if($icon)
-            <div class="text-{{ $color }}-500">
-                {!! $icon !!}
-            </div>
-        @endif
+    <!-- Title -->
+    <h3 class="text-sm text-gray-500 dark:text-gray-300 mb-2">{{ $title }}</h3>
 
-    </div>
-
+    <!-- Value -->
+    <p class="text-2xl font-bold text-gray-900 dark:text-white">{{ $value}}</p>
 </div>
 
-________________________________________
+@if($route)
+</a>
+@endif
